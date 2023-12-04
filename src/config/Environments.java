500c8d57589182ac0f0d1f0221920f88ddfe2219
Alex <figarozxc17@gmail.com>
2023-12-04 19:04:53 -0400

    first-commit

---
+++ src/config/Environments.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Environments {
+    public static String lag = "en";
+    public static String pathFile = "\\App\\data\\user.txt";
+    public static String pathFileProducts = "\\App\\data\\product.txt";
+}
